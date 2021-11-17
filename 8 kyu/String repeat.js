25556c14beb769f98de57ef9c0aa232069a37ac0
Leo <kovzel.leonid@mail.ru>
2021-11-17 13:29:19 +0300

    more 8 kyu solved(part 4)

--- 8 kyu/String repeat.js
+++ 8 kyu/String repeat.js
@@ -1,3 +1,13 @@
 function repeatStr (n, s) {
   return s.repeat(n);
 }
+
+
+function confirmEnding(str, target) {
+    let endStr = str.split('').pop();
+    console.log(endStr)
+  if (str.split('').pop() == target) return true;
+  else return false
+}
+
+confirmEnding("Bastian", "n");
