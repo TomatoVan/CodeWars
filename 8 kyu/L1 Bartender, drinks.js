214de4d12cb5ec991db0d1d634594b7038ecebc2
Leo <kovzel.leonid@mail.ru>
2022-01-16 13:16:56 +0300

    More 8kyu tasks

---
+++ 8 kyu/L1 Bartender, drinks.js
@@ -0,0 +1,10 @@
+const table = {
+	jabroni: 'Patron Tequila',
+	'school counselor': 'Anything with Alcohol',
+	programmer: 'Hipster Craft Beer',
+	'bike gang member': 'Moonshine',
+	politician: 'Your tax dollars',
+	rapper: 'Cristal',
+}
+
+const getDrinkByProfession = parameter => table[parameter.toLowerCase()] || 'Beer'
