76f13ed5fb3d7b0f5ac025279cf4b06c549ffdad
Leo <kovzel.leonid@mail.ru>
2022-01-30 17:36:08 +0300

    More 8kyu tasks

---
+++ 8 kyu/Determine offspring sex based on genes XX and XY chromosomes.js
@@ -0,0 +1,4 @@
+function chromosomeCheck(sperm) {
+	if (sperm === 'XY') return "Congratulations! You're going to have a son."
+	if (sperm === 'XX') return "Congratulations! You're going to have a daughter."
+}
